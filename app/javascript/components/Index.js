07ee4235fb5c99b7a2686e9caee19a089e3557c5
Shawn <codingshawn@gmail.com>
2020-12-20 17:17:16 +0800

    Initial commit

---
+++ app/javascript/components/Index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Navbar from "./Navbar"
+import PropTypes from "prop-types"
+
+class Index extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <Navbar/>
+        Index World!!!!!!!!
+      </React.Fragment>
+    );
+  }
+}
+
+export default Index
