9346d848d3c197d1cd9559bff40701301b7234d2
Shawn <codingshawn@gmail.com>
2020-12-25 21:21:05 +0800

    Added controller method to check if user found togepi

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,22 @@
 class MapsController < ApplicationController
+  TOGEPICOORDS = [11.02, 94.3]
+
   def index
   end 
+
+  def checkImage(correctX, correctY, x, y)
+    return x <= correctX + 2.5 && x >= correctX - 2.5 && 
+            y <= correctY + 2.5 && y >= correctY - 2.5
+  end
+
+  def checktogepi
+    @status = checkImage(TOGEPICOORDS[0], TOGEPICOORDS[1], params[:x].to_i , params[:y].to_i)
+    render :json => {checkStatus: @status}
+  end
+
+  private
+  def coords_params
+    params.permit(:x, :y)
+  end
+
 end
