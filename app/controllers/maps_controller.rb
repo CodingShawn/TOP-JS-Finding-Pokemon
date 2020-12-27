dc7968e715424be926445f6c8a52170bba5beecc
Shawn <codingshawn@gmail.com>
2020-12-27 17:20:41 +0800

    Added methods to controller for checking if targeted pokemon is found

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,16 +1,28 @@
 class MapsController < ApplicationController
-  TOGEPICOORDS = [11.02, 94.3]
+  TOGEPICOORDS = [9, 95]
+  MAGIKARPCOORDS = [87.8, 39.6]
+  PORYGONCOORDS = [56.6, 51.8]
 
   def index
   end 
 
   def checkImage(correctX, correctY, x, y)
-    return x <= correctX + 2.5 && x >= correctX - 2.5 && 
-            y <= correctY + 2.5 && y >= correctY - 2.5
+    return x <= correctX + 2 && x >= correctX - 2 && 
+            y <= correctY + 2 && y >= correctY - 2
   end
 
   def checktogepi
     @status = checkImage(TOGEPICOORDS[0], TOGEPICOORDS[1], params[:x].to_i , params[:y].to_i)
+    render :json => {checkStatus: @status}
+  end
+
+  def checkmagikarp
+    @status = checkImage(MAGIKARPCOORDS[0], MAGIKARPCOORDS[1], params[:x].to_i, params[:y].to_i)
+    render :json => {checkStatus: @status}
+  end
+
+  def checkporygon
+    @status = checkImage(PORYGONCOORDS[0], PORYGONCOORDS[1], params[:x].to_i, params[:y].to_i)
     render :json => {checkStatus: @status}
   end
 
