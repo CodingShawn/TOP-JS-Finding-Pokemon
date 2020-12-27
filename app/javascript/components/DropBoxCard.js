d7058908767a6c198ae82ef992ade5b37890e22a
Shawn <codingshawn@gmail.com>
2020-12-27 17:08:38 +0800

    Edited formating of DropBoxCard component as well as gave card an id for CSS styling

--- app/javascript/components/DropBoxCard.js
+++ app/javascript/components/DropBoxCard.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 
 function DropBoxCard(props) {
-  let {cardImage, cardDescription } = props
+  let { cardImage, cardDescription, checkPosition } = props;
+
   return (
-    <div className="drop-box-card">
+    <div
+      className="drop-box-card"
+      id={cardDescription + "-card"}
+      onClick={() => checkPosition(cardDescription)}
+    >
       <div className="card-image-wrapper">
-        <img className={cardDescription} src={cardImage}/>
+        <img className={cardDescription} src={cardImage} />
       </div>
-      <div className="card-description">
-        {cardDescription.toUpperCase()} 
-      </div>
+      <div className="card-description">{cardDescription.toUpperCase()}</div>
     </div>
-  )
+  );
 }
 
-export default DropBoxCard
+export default DropBoxCard;
