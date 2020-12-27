759dbafa635a3cadcac111dced2a516dfd31afa6
Shawn <codingshawn@gmail.com>
2020-12-27 17:23:47 +0800

    Added routing to maps controller to allow checking for targeted pokemon

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'maps#index'
+  get 'maps/checktogepi', to: 'maps#checktogepi'
+  get 'maps/checkmagikarp', to: 'maps#checkmagikarp'
+  get 'maps/checkporygon', to: 'maps#checkporygon'
 end
