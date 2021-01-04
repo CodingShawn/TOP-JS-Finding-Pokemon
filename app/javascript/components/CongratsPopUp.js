3a3abeee56c995704bb1306d770ec4ab3afa29e9
Shawn <codingshawn@gmail.com>
2021-01-04 22:04:48 +0800

    Updated Congrats popup to allow user to submit name to hall of fame. After submitting will show top 5 scorers

--- app/javascript/components/CongratsPopUp.js
+++ app/javascript/components/CongratsPopUp.js
@@ -3,8 +3,51 @@ import { formatTime } from "../util";
 
 function CongratsPopUp(props) {
   let [playerName, setPlayerName] = useState("");
+  let [topScorers, setTopScorers] = useState(null);
   function handleChange(event) {
     setPlayerName(event.target.value);
+  }
+
+  function submitScore(event) {
+    event.preventDefault();
+    let url = "/players";
+    const csrf = document
+      .querySelector("meta[name='csrf-token']")
+      .getAttribute("content");
+    let data = {
+      playerName: playerName,
+      score: props.currentTime,
+    };
+    let otherParam = {
+      headers: { "X-CSRF-Token": csrf, "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+      method: "POST",
+    };
+    sendSubmitScoreRequest(url, otherParam);
+  }
+
+  async function sendSubmitScoreRequest(url, otherParam) {
+    let response = await fetch(url, otherParam);
+    let { topscorers: returnDataArray } = await response.json();
+    setTopScorers(returnDataArray);
+  }
+
+  function TopScorersReceived() {
+    if (topScorers) {
+      return (
+        <div className="top-scorer-wrapper">
+          <h2>Best Times</h2>
+          {topScorers.map((topScorer, index) => (
+            <div className="top-scorer">
+              <div>
+                {index + 1}. {topScorer.name}
+              </div>{" "}
+              <div>{formatTime(topScorer.score)}</div>
+            </div>
+          ))}
+        </div>
+      );
+    } else return null;
   }
 
   return (
@@ -12,18 +55,22 @@ function CongratsPopUp(props) {
       <div className="congrats-wrapper">
         <h1>You have found all the lost Pokemon!</h1>
         <div>Time Taken: {formatTime(props.currentTime)}</div>
-        <form>
-          <label>
-            Enter your name to the Hall of Fame:
-            <br />
-            <input
-              type="text"
-              value={playerName}
-              onChange={handleChange}
-            ></input>
-          </label>
-          <button>Enter</button>
-        </form>
+        {!topScorers && (
+          <form className="score-form">
+            <label>
+              Enter your name to the Hall of Fame:
+              <br />
+              <input
+                type="text"
+                value={playerName}
+                onChange={handleChange}
+                required
+              ></input>
+            </label>
+            <button onClick={submitScore}>Enter</button>
+          </form>
+        )}
+        <TopScorersReceived />
       </div>
     </div>
   );
