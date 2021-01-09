cf9a62e201366990d605ced8aae0d80e090a0957
Shawn <codingshawn@gmail.com>
2021-01-09 23:17:09 +0800

    Commit index react class

---
+++ app/javascript/components/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import App from './App';
+
+class index extends React.Component {
+  render() {
+    return (
+      <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    )
+  }
+}
+
+export default index
